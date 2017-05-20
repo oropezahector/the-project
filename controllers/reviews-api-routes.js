f637a7b4173be6b3672659a700a73d8ebc587c57
dangnabit <dan.gabel@gmail.com>
2017-05-20 11:34:55 -0500

    app.js init

--- controllers/reviews-api-routes.js
+++ controllers/reviews-api-routes.js
@@ -2,7 +2,7 @@ var models = require('../models');
 
 module.exports = function(app) {
 
-  app.get('/api/reviews', function(req, res) {
+  app.get('/api/review', function(req, res) {
     models.Reviews.findAll({
       include: [{
         model: models.User
@@ -12,7 +12,7 @@ module.exports = function(app) {
     });
   });
 
-  app.get('/api/reviews/:id', function(req, res) {
+  app.get('/api/review/:id', function(req, res) {
     models.Reviews.findOne({
       where: {
         id: req.params.id
@@ -25,13 +25,13 @@ module.exports = function(app) {
     });
   });
 
-  app.post('/api/reviews', function(req, res) {
+  app.post('/api/review', function(req, res) {
     db.Review.create(req.body).then(function(reviews) {
       res.json(reviews);
     });
   });
 
-  app.put('/api/reviews/:id', function(req, res){
+  app.put('/api/review/:id', function(req, res){
     db.Review.update(
       req.body,{
         where: {
--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
+  function getDataID(id, type) {
+    var queryUrl;
+    switch (type) {
+      case "user":
+        queryUrl = "/api/users/" + id;
+        break;
+      case "building":
+        queryUrl = "/api/buildings/" + id;
+        break;
+      default:
+        return;
+    }
+    $.get(queryUrl, function(data) {
+      if (data) {
+        console.log(data.UserID || data.id)
+        userId = data.UserID || data.id;
+      }
+    });
+  }
+
+  function getUsers() {
+    $.get("/api/user", renderUserList);
+  }
+
+  function renderUserList(data) {
+    if (!data.length) {
+      window.location.href = "/";
+    } else {
+      console.log(data);
+    }
+  }
+
+  function getBuildings() {
+    $.get("/api/building", renderBuildings);
+  }
+
+  function renderBuildings(data) {
+    if (!data.length) {
+      window.location.href = "/";
+    } else {
+      console.log(data);
+    }
+  }
 
 
 
