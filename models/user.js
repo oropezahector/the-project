71c133a4a100dd9add02096b7381fbdd0e01e01c
Sam Malefyt <samanthadwm@gmail.com>
2017-05-20 10:18:10 -0500

    updated user model

--- models/user.js
+++ models/user.js
@@ -2,7 +2,6 @@ module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     name: DataTypes.STRING,
     fb_id: DataTypes.STRING
-    post_count: DataTypes.INTEGER;
   },
   },
     {
