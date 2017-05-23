3462b2d88b8b75fb7d7e0ed581ae090c62b00db2
Sam Malefyt <samanthadwm@gmail.com>
2017-05-22 20:27:14 -0500

    updated building.js

--- models/building.js
+++ models/building.js
@@ -1,15 +1,14 @@
 module.exports = function(sequelize, DataTypes) {
   var Building = sequelize.define("Building", {
-    address: {
+    place_id: {
       type: DataTypes.STRING,
-      allowNull: false,
       primaryKey: true
     },
-    ratings: DataTypes.STRING,
-    googleMaps_id: {
+    address: {
       type: DataTypes.STRING,
-      allowNull: true
-  },
+      allowNull: false
+    },
+    ratings: DataTypes.STRING,
     {
       classMethods: {
         associate: function(models) {
