5f99d59295d324acc06d0627502b808fc4e34e42
Sam Malefyt <samanthadwm@gmail.com>
2017-05-22 19:46:05 -0500

    jawsdb connection

---
+++ server.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+
+var connection; 
+if (process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection(appConfig.config.development);
+};
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
