4a2cd931ac99dc36fa735293f2e77dc6793d9fa7
dangnabit <dan.gabel@gmail.com>
2017-06-03 09:27:09 -0500

    revert reload

--- public/js/app.js
+++ public/js/app.js
@@ -335,7 +335,7 @@ $(document).ready(function() {
         getDataID(address.data('placeid'), 'building');
         reviewForm.addClass('hidden');
         reviewBtn.removeClass('hidden');
-        window.location.reload();
+        
       });
     } else {
       $('#error').modal('toggle');
